199693d20321e1199936a9d77b9f522a409927a5
Patrick Froch <patrick.froch@easySolutionsIT.de>
2020-11-15 10:21:29 +0100

    INITIALER COMMIT

---
+++ Classes/Contao/Manager/Plugin.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types = 1);
+/**
+ * @package     enlighter
+ * @filesource  Plugin.php
+ * @version     1.0.0
+ * @since       15.11.2020 - 09:56
+ * @author      Patrick Froch <info@easySolutionsIT.de>
+ * @see        http://easySolutionsIT.de
+ * @copyright   e@sy Solutions IT 2020
+ * @license     EULA
+ */
+namespace Esit\Enlighter\Classes\Contao\Manager;
+
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+/**
+ * Class Plugin
+ * @package Esit\Enlighter\Classes\Contao\Manager
+ */
+class Plugin implements BundlePluginInterface
+{
+
+
+    /**
+     * @param  ParserInterface                                             $parser
+     * @return array|\Contao\ManagerPlugin\Bundle\Config\ConfigInterface[]
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(\Esit\Enlighter\EsitEnlighterBundle::class)
+                ->setLoadAfter([\Contao\CoreBundle\ContaoCoreBundle::class])
+        ];
+    }
+}
---
+++ EsitEnlighterBundle.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+/**
+ * @package     enlighter
+ * @filesource  EsitEnlighterBundle.php
+ * @version     1.0.0
+ * @since       15.11.2020 - 09:56
+ * @author      Patrick Froch <info@easySolutionsIT.de>
+ * @link        http://easySolutionsIT.de
+ * @copyright   e@sy Solutions IT 2020
+ * @license     EULA
+ */
+namespace Esit\Enlighter;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class EsitEnlighterBundle
+ * @package Esit\Enlighter
+ */
+class EsitEnlighterBundle extends Bundle
+{
+}
